1d7a47abc96c20f2d940783684760d6557155929
Lexaa19 <55296361+Lexaa19@users.noreply.github.com>
2021-08-11 08:53:31 +0300

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -4,32 +4,41 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PhoneBook {
-	// key: last name, value: first name
-	private Map<String, String> contactFullNameMap = new HashMap<String, String>();
-	// phoneNumber must be unique
-	private Map<String, String> phoneNumberMap = new HashMap<String, String>();
+	// key: phone number
+	private Map<String, Contact> contactMap = new HashMap<String, Contact>();
 
 	public void add(Contact entry) {
-		this.contactFullNameMap.put(entry.getLastName(), entry.getFirstName());
-		this.phoneNumberMap.put(entry.getPhoneNumber(), entry.getLastName());
+		this.contactMap.put(entry.getPhoneNumber(), new Contact(entry.getPhoneNumber(), entry.getFirstName(),
+				entry.getLastName(), entry.getEmail(), entry.getAddress()));
 	}
 
-	public Map<String, String> getPhoneNumberMap() {
-		return new HashMap<String, String>(phoneNumberMap);
+	public Map<String, Contact> getContactMap() {
+		return new HashMap<String, Contact>(contactMap);
 	}
 
-	public void setPhoneNumberMap(Map<String, String> phoneNumberMap) {
-		this.phoneNumberMap = phoneNumberMap;
+	public void setContactMap(Map<String, Contact> contactMap) {
+		this.contactMap = contactMap;
 	}
 
-	public Map<String, String> getContactFullNameMap() {
-		return new HashMap<String, String>(contactFullNameMap);
+	public void updateContact(Contact entry) {
+		this.contactMap.replace(entry.getPhoneNumber(), new Contact(entry.getPhoneNumber(), entry.getFirstName(),
+				entry.getLastName(), entry.getEmail(), entry.getAddress()));
 	}
 
-	public void setContactFullNameMap(Map<String, String> contactFullNameMap) {
-		this.contactFullNameMap = contactFullNameMap;
+	public boolean isValidMobileNo(String str) {
+	    // 1) Begins with 0 or 07
+	    // 2) The rest 7 digits can contain any number between 0 to 9
+	    Pattern p = Pattern.compile("(0|07)?[7][0-9]{8}");
+	 
+	    // Pattern class contains matcher() method
+	    // to find matching between given number
+	    // and regular expression
+	    Matcher m = p.matcher(str);
+	    return (m.find() && m.group().equals(str));
 	}
 
 }
