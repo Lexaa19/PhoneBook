63026e4ba2bd4994f011c52afc28f5881c4cf3ff
Lexaa19 <55296361+Lexaa19@users.noreply.github.com>
2021-08-11 12:39:51 +0300

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -29,23 +29,40 @@ public class PhoneBook {
 				entry.getLastName(), entry.getEmail(), entry.getAddress()));
 	}
 
+	/**
+	 * 
+	 * @param str -> the string to be check by the regex
+	 * @return true if the string matches the regex pattern and false otherwise
+	 */
 	public boolean isValidMobileNo(String str) {
-		// check if number starts with 0
-		Pattern p = Pattern.compile("^0[0-9-( )]+$");
 
-		// Pattern class contains matcher() method
-		// to find matching between given string
-		// and regular expression
-		// remove the white spaces and dashes from the string so that they are not
-		// counted when computing length
-		str = str.replaceAll("[()\\s-]+", "");
-		Matcher m = p.matcher(str);
+		/*
+		 * Pattern class contains matcher() method to find matching between given string
+		 * and regular expression
+		 * 
+		 * ^ defines that the pattern must start at beginning of a new line
+		 * 
+		 * by using ^ and $ you tell the engine that whatever is in between them must
+		 * cover the entire line end-to-end
+		 */
 
-		if ((m.find() && m.group().equals(str)) && str.length() == 10) {
-			return true;
-		} else {
-			return false;
+		try {
+			Pattern p = Pattern.compile("^0[0-9-( )]+$");
+
+			// remove the white spaces and dashes from the string so that they are not
+			// counted when computing length
+			str = str.replaceAll("[()\\s-]+", "");
+			Matcher m = p.matcher(str);
+			if ((m.find() && m.group().equals(str)) && str.length() == 10) {
+				return true;
+			} else {
+				return false;
+			}
+		} catch (PatternSyntaxException ex) {
+			System.out.println("The phone number string format is not correct: " + ex.getPattern());
+			System.out.println(ex.getMessage());
 		}
+		return true;
 	}
 
 }
