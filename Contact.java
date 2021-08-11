64d36ad6b629be19215d871b0a94b7dfa0f91a97
Lexaa19 <55296361+Lexaa19@users.noreply.github.com>
2021-08-11 08:51:57 +0300

    Update Contact.java

--- Contact.java
+++ Contact.java
@@ -5,11 +5,15 @@ class Contact {
 	private String phoneNumber;
 	private String firstName;
 	private String lastName;
+	private String email;
+	private String address;
 
-	public Contact(String firstName, String lastName, String phoneNumber) {
+	public Contact(String phoneNumber, String firstName, String lastName, String email, String address) {
 		this.phoneNumber = phoneNumber;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		this.email = email;
+		this.address = address;
 	}
 
 	public String getPhoneNumber() {
@@ -24,6 +28,14 @@ class Contact {
 		return lastName;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
@@ -36,4 +48,19 @@ class Contact {
 		this.lastName = lastName;
 	}
 
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Contact [phoneNumber=" + phoneNumber + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", email=" + email + ", address=" + address + "]" ;
+	}
+
 }
+
