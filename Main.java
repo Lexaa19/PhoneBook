8b3fec3a7f556164597fb57de9cf9063ddbb1a7a
Lexaa19 <55296361+Lexaa19@users.noreply.github.com>
2021-08-17 15:41:48 +0300

    Added the search by first name, last name, code improvements

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Map.Entry;
 
 public class Main {
@@ -59,10 +56,7 @@ public class Main {
 			case OPTION_FIND_NAME:
 				System.out.println("Find a contact by name: ");
 				firstName = scanner.nextLine();
-				// String[] a = scanner.nextLine().split(" ");
-
 				searchContactByName(phoneBook, firstName);
-
 				break;
 
 			case OPTION_EXIT:
@@ -77,32 +71,7 @@ public class Main {
 
 	}
 
-	// method used in case 4 to search by first name or last name
-	public static void searchContactByName(PhoneBook phonebook, String name) {
-		Scanner scanner = new Scanner(System.in);
-
-		Iterator<Map.Entry<String, Contact>> contactMapIterator = phonebook.getContactMap().entrySet().iterator();
-		while (contactMapIterator.hasNext()) {
-			Entry<String, Contact> contact = contactMapIterator.next();
-			if (name.equals(contact.getValue().getFirstName()) || name.equals(contact.getValue().getLastName())) {
-				System.out.println("Phone number: " + contact.getValue().getPhoneNumber());
-				System.out.println("First name: " + contact.getValue().getFirstName());
-				System.out.println("Last name: " + contact.getValue().getLastName());
-				System.out.println("Email: " + contact.getValue().getEmail());
-				System.out.println("Address: " + contact.getValue().getAddress());
-			} else if (name.equals(contact.getValue().getLastName())) {
-				System.out.println("Phone number: " + contact.getValue().getPhoneNumber());
-				System.out.println("First name: " + contact.getValue().getFirstName());
-				System.out.println("Last name: " + contact.getValue().getLastName());
-				System.out.println("Email: " + contact.getValue().getEmail());
-				System.out.println("Address: " + contact.getValue().getAddress());
-			}
-		}
-		System.out.println("\nPress ENTER to continue.");
-		scanner.nextLine();
-	}
-
-	// SWITCH CASES SPLIT INTO METHODS
+	// SWITCH CASES SPLIT INTO METHODS and their auxiliary methods
 	public static void caseAddEdit(String phoneNumber, String firstName, String lastName, String email, String address,
 			PhoneBook phoneBook) {
 		Scanner scanner = new Scanner(System.in);
@@ -150,19 +119,75 @@ public class Main {
 		scanner.nextLine();
 	}
 
+	// method used in case 2 to view all the contacts
+	public static void getContactMapEntries(PhoneBook phonebook) {
+		for (Map.Entry<String, Contact> contact : phonebook.getContactMap().entrySet()) {
+			Contact contactMapValue = contact.getValue();
+			// %s is a 'format character', indicating "insert a string here"
+			System.out.println(String.format("%s %s %s %s ", contactMapValue.getPhoneNumber(),
+					contactMapValue.getFirstName(), contactMapValue.getLastName(), contactMapValue.getEmail()));
+		}
+	}
+
 	public static void caseFindNumber(PhoneBook phoneBook) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Search by phone number: ");
 		String phoneNumber = scanner.nextLine();
+
 		while (phoneNumber.isEmpty()) {
 			System.out.print("The phone number cannot be empty: ");
 			phoneNumber = scanner.nextLine();
-
 		}
 
 		if (!phoneNumber.isEmpty()) {
 			searchContactByPhoneNumber(phoneBook, phoneNumber);
 		}
+	}
+
+	/*
+	 * Method used in case 3 to search by phone number
+	 * 
+	 * Map.Entry interface in Java provides certain methods to access the entry in
+	 * the Map. Map.Entry interface in Java provides certain methods to access the
+	 * entry in the Map. It should be used if you need both map keys and values in
+	 * the loop
+	 * 
+	 * entrySet() method in Java is used to create a set out of the same elements
+	 * contained in the hash map. It basically returns a set view of the hash map or
+	 * we can create a new set and store the map elements into them.
+	 * 
+	 * entrySet() is doing a lookup only once, so it is faster
+	 * 
+	 * The value found is used for outputting the right message if the phone number
+	 * was not found. Initially set to false, when the number is found its value is
+	 * true
+	 *
+	 */
+
+	public static void searchContactByPhoneNumber(PhoneBook phonebook, String phoneNumber) {
+		Scanner scanner = new Scanner(System.in);
+		boolean found = false;
+		Iterator<Map.Entry<String, Contact>> contactMapIterator = phonebook.getContactMap().entrySet().iterator();
+		while (contactMapIterator.hasNext()) {
+			Entry<String, Contact> contact = contactMapIterator.next();
+			if (phoneNumber.equals(contact.getKey())) {
+				outputSearchByPhone(phonebook, contact);
+				found = true;
+			}
+		}
+		if (!found) {
+			System.out.println("The phone number could not be found in the address book.");
+		}
+		System.out.println("\nPress ENTER to continue.");
+		scanner.nextLine();
+	}
+
+	public static void outputSearchByPhone(PhoneBook phonebook, Entry<String, Contact> contactEntry) {
+		System.out.println("Phone number: " + contactEntry.getValue().getPhoneNumber());
+		System.out.println("First name: " + contactEntry.getValue().getFirstName());
+		System.out.println("Last name: " + contactEntry.getValue().getLastName());
+		System.out.println("Email: " + contactEntry.getValue().getEmail());
+		System.out.println("Address: " + contactEntry.getValue().getAddress());
 	}
 
 	// METHODS FOR READING USER INPUT
@@ -220,49 +245,85 @@ public class Main {
 		return address;
 	}
 
-	// method used in case 2 to view all the contacts
-	public static void getContactMapEntries(PhoneBook phonebook) {
-		for (Map.Entry<String, Contact> contact : phonebook.getContactMap().entrySet()) {
-			Contact contactMapValue = contact.getValue();
-			// %s is a 'format character', indicating "insert a string here"
-			System.out.println(String.format("%s %s %s %s ", contactMapValue.getPhoneNumber(),
-					contactMapValue.getFirstName(), contactMapValue.getLastName(), contactMapValue.getEmail()));
-		}
-	}
-
-	/*
-	 * Method used in case 3 to search by phone number
+	/**
+	 * Method used in case 4 to search by first name or last name.
+	 * 
+	 * -----Theory Introduction-----
+	 * 
+	 * Since the map doesn’t extend the Collection interface, it doesn’t have its
+	 * own iterator. But Map.entrySet() returns a set of key-value mappings.
 	 * 
 	 * Map.Entry interface in Java provides certain methods to access the entry in
-	 * the Map. Map.Entry interface in Java provides certain methods to access the
-	 * entry in the Map. It should be used if you need both map keys and values in
-	 * the loop
+	 * the Map. (for key-value pair)
 	 * 
 	 * entrySet() method in Java is used to create a set out of the same elements
 	 * contained in the hash map. It basically returns a set view of the hash map or
-	 * we can create a new set and store the map elements into them.
+	 * we can create a new set and store the map elements into them. entrySet() is
+	 * faster
 	 * 
-	 * entrySet() is doing a lookup only once, so it is faster
+	 * -----Method explanation-----
+	 * 
+	 * The method searches for the name entered by the user in the following
+	 * scenarios: 1. The user enters the full first name or the full last name or
+	 * both 2. The user enters the first name or the last name partially ("Jo"
+	 * instead of "John")
+	 * 
+	 * In order to check for the first name or last name we have to split the user
+	 * input (using the isSpaceInContactName(String name) method) taking into
+	 * account the white spaces. We split the String by space and access the first
+	 * name as the element at index 0 and the last name as the element at index 1
+	 * 
+	 * 
+	 * @param PhoneBook phonebook -> the Phone book where we have the contacts,
+	 *                  String name -> the name we search for
 	 */
-
-	public static void searchContactByPhoneNumber(PhoneBook phonebook, String phoneNumber) {
+	public static void searchContactByName(PhoneBook phonebook, String name) {
 		Scanner scanner = new Scanner(System.in);
+		boolean foundContact = false;
+
+		// phonebook.getContactMap() returns all the contacts in the phone book
 		Iterator<Map.Entry<String, Contact>> contactMapIterator = phonebook.getContactMap().entrySet().iterator();
 		while (contactMapIterator.hasNext()) {
 			Entry<String, Contact> contact = contactMapIterator.next();
-			if (phoneNumber.equals(contact.getKey())) {
-				System.out.println("Phone number: " + contact.getValue().getPhoneNumber());
-				System.out.println("First name: " + contact.getValue().getFirstName());
-				System.out.println("Last name: " + contact.getValue().getLastName());
-				System.out.println("Email: " + contact.getValue().getEmail());
-				System.out.println("Address: " + contact.getValue().getAddress());
-			}else {
-				System.out.println("The phone number could not be found in the address book.");
-			}
+			String contactFirstNameToString = String.valueOf(contact.getValue().getFirstName());
+			String contactLastNameToString = String.valueOf(contact.getValue().getLastName());
 
+			// search by either first name or last name
+			if (contactFirstNameToString.contains(name) || contactLastNameToString.contains(name)) {
+				// output contact details when found in the phone book
+				outputSearchByName(phonebook, contact);
+				foundContact = true;
+			}
+			if (isSpaceInContactName(name)) {
+				// split the user input name
+				String[] entries = name.split(" ");
+				// check for both first name and last name in the phone book
+				if (contactFirstNameToString.contains(entries[0]) && contactLastNameToString.contains(entries[1])) {
+					foundContact = true;
+					// output contact details when found in the phone book
+					outputSearchByName(phonebook, contact);
+				}
+			}
+		}
+		// if the keyword is not found, return this message
+		if (!foundContact) {
+			System.out.println("No results.");
 		}
 		System.out.println("\nPress ENTER to continue.");
 		scanner.nextLine();
+	}
+
+	public static boolean isSpaceInContactName(String name) {
+		// split the String when white space is found
+		Pattern pattern = Pattern.compile("\\s");
+		Matcher matcher = pattern.matcher(name);
+		boolean found = matcher.find();
+		return found;
+	}
+
+	public static void outputSearchByName(PhoneBook phonebook, Entry<String, Contact> contactEntry) {
+		System.out.println(contactEntry.getValue().getPhoneNumber() + " " + contactEntry.getValue().getFirstName() + " "
+				+ contactEntry.getValue().getLastName() + " " + contactEntry.getValue().getEmail());
 	}
 
 }
