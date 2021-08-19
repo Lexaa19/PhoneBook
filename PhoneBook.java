4b3f7e10c56299ebcf0950576df925f2bf2c92a2
Lexaa19 <55296361+Lexaa19@users.noreply.github.com>
2021-08-19 08:38:31 +0300

    added JavaDoc

--- PhoneBook.java
+++ PhoneBook.java
@@ -7,12 +7,13 @@ import java.util.regex.PatternSyntaxException;
 /**
  * Represents the PhoneBook containing all the contacts.
  * 
- * It also holds the logic for the contact operations(add, update etc)
+ * It also holds the logic for the contact operations (add, update etc.).
  * 
  * It doesn't hold any messages that are displayed to the user. That is done in
- * the Main class
+ * the Main class.
  * 
  * @author Caliopi Durdunescu
+ * @version 1
  * @since 17.08.2021
  */
 public class PhoneBook {
@@ -20,83 +21,87 @@ public class PhoneBook {
 	private Map<String, Contact> contactMap = new HashMap<>();
 
 	/**
-	 * Uses the Contact class copy constructor to add contacts to the contactMap
+	 * Uses the Contact class copy constructor to add contacts to the contactMap.
 	 * 
-	 * @param entry an object of type Contact
+	 * @param entry An object of type Contact.
 	 */
 	public void add(Contact entry) {
 		this.contactMap.put(entry.getPhoneNumber(), entry);
 	}
 
 	/**
-	 * Gets a copy (using the "new" keyword") of the contactMap containing all the
-	 * contacts
+	 * Gets the contactMap containing all the contacts.
+	 * 
+	 * @return A copy of the contactMap.
 	 */
 	public Map<String, Contact> getContactMap() {
 		return new HashMap<String, Contact>(contactMap);
 	}
 
 	/**
-	 * Updates an entry in the contactMap
+	 * Updates an entry in the contactMap.
 	 * 
-	 * @param entry an object of type Contact
+	 * @param entry An object of type Contact.
 	 */
 	public void updateContact(Contact entry) {
 		this.contactMap.replace(entry.getPhoneNumber(), entry);
 	}
 
 	/**
-	 * Checks if the phone number is a valid one with the help of regex
+	 * Checks if the phone number is valid with the help of Regex. It also returns
+	 * the normalized form of the phone number.
 	 * 
-	 * @param str the string to be checked by the regex
-	 * @return true if the string matches the regex pattern
+	 * 07 (70) 19 -6645 becomes 0770196645.This will help with the search by phone
+	 * number.
+	 * 
+	 * @param phoneNumber The phone number from user input to be checked by the
+	 *                    Regex.
+	 * @return The phone number in a normalized form.
 	 */
-	public boolean isValidMobileNo(String str) {
+	public String isValidMobileNo(String phoneNumber) {
 		/*
-		 * Pattern class contains matcher() method to find matching between given string
-		 * and regular expression
+		 * Pattern class contains the matcher() method to find the matching between the
+		 * given string and the regular expression.
 		 * 
-		 * ^ defines that the pattern must start at the beginning of a new line
+		 * ^ Defines that the pattern must start at the beginning of a new line.
 		 * 
-		 * by using ^ and $ you tell the engine that whatever is in between them must
-		 * cover the entire line end-to-end
+		 * By using ^ and $ you tell the engine that whatever is in between these signs
+		 * must be covered.
 		 */
 
 		try {
-			Pattern p = Pattern.compile("^0[0-9-( )]+$");
+			Pattern phonePattern = Pattern.compile("^0[0-9-( )]+$");
 
 			/*
-			 * remove the white spaces and dashes from the string so that they are not
-			 * counted when computing length.
+			 * Remove the white spaces and dashes from the string so that they are not
+			 * counted when computing the phone number length.
 			 * 
-			 * + => One or more
+			 * + => One or more.
 			 * 
-			 * \\s => empty space
+			 * \\s => Empty space.
 			 */
-			str = str.replaceAll("[()\\s-]+", "");
+			phoneNumber = phoneNumber.replaceAll("[()\\s-]+", "");
 
 			/*
-			 * returns a Matcher object which contains information about the search that was
-			 * performed
+			 * Returns a Matcher object which contains information about the search that was
+			 * performed.
 			 */
 
-			Matcher m = p.matcher(str);
+			Matcher phoneMatcher = phonePattern.matcher(phoneNumber);
+
 			/*
-			 * groups are a way to treat multiple characters as a single unit.
+			 * Groups are a way to treat multiple characters as a single unit.
 			 * 
-			 * The find() method of Matcher Class attempts to find the next subsequence of
-			 * the input sequence that find the pattern. It returns a boolean value
+			 * The find() method of Matcher Class. It returns a boolean value.
 			 */
-			if ((m.find() && m.group().equals(str)) && str.length() == 10) {
-				return true;
-			} else {
-				return false;
+			if ((phoneMatcher.find() && phoneMatcher.group().equals(phoneNumber)) && phoneNumber.length() == 10) {
+				return phoneNumber;
 			}
 		} catch (PatternSyntaxException ex) {
 			System.out.println("The phone number string format is not correct: " + ex.getPattern());
 			System.out.println(ex.getMessage());
 		}
-		return true;
+		return phoneNumber;
 	}
 
 }
